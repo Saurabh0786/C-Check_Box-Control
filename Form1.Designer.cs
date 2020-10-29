f1edd4dac46d13e0b1e1bfb8a9dbc6c37364acb7
Saurabh0786 <40481420+Saurabh0786@users.noreply.github.com>
2020-10-29 18:57:55 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Check_Box
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboneplus = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbapple = new System.Windows.Forms.CheckBox();
+            this.cbsamsung = new System.Windows.Forms.CheckBox();
+            this.cbvivo = new System.Windows.Forms.CheckBox();
+            this.cboppo = new System.Windows.Forms.CheckBox();
+            this.cbxiaomi = new System.Windows.Forms.CheckBox();
+            this.cbrealme = new System.Windows.Forms.CheckBox();
+            this.cbrog = new System.Windows.Forms.CheckBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnmybutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cboneplus
+            // 
+            this.cboneplus.AutoSize = true;
+            this.cboneplus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboneplus.Location = new System.Drawing.Point(27, 82);
+            this.cboneplus.Name = "cboneplus";
+            this.cboneplus.Size = new System.Drawing.Size(112, 28);
+            this.cboneplus.TabIndex = 0;
+            this.cboneplus.Text = "OnePlus";
+            this.cboneplus.UseVisualStyleBackColor = true;
+            this.cboneplus.CheckedChanged += new System.EventHandler(this.cboneplus_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(374, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Your Favourite Mobile Company";
+            // 
+            // cbapple
+            // 
+            this.cbapple.AutoSize = true;
+            this.cbapple.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbapple.Location = new System.Drawing.Point(27, 133);
+            this.cbapple.Name = "cbapple";
+            this.cbapple.Size = new System.Drawing.Size(87, 28);
+            this.cbapple.TabIndex = 2;
+            this.cbapple.Text = "Apple";
+            this.cbapple.UseVisualStyleBackColor = true;
+            this.cbapple.CheckedChanged += new System.EventHandler(this.cbapple_CheckedChanged);
+            // 
+            // cbsamsung
+            // 
+            this.cbsamsung.AutoSize = true;
+            this.cbsamsung.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbsamsung.Location = new System.Drawing.Point(27, 186);
+            this.cbsamsung.Name = "cbsamsung";
+            this.cbsamsung.Size = new System.Drawing.Size(119, 28);
+            this.cbsamsung.TabIndex = 3;
+            this.cbsamsung.Text = "Samsung";
+            this.cbsamsung.UseVisualStyleBackColor = true;
+            this.cbsamsung.CheckedChanged += new System.EventHandler(this.cbsamsung_CheckedChanged);
+            // 
+            // cbvivo
+            // 
+            this.cbvivo.AutoSize = true;
+            this.cbvivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbvivo.Location = new System.Drawing.Point(27, 247);
+            this.cbvivo.Name = "cbvivo";
+            this.cbvivo.Size = new System.Drawing.Size(73, 28);
+            this.cbvivo.TabIndex = 4;
+            this.cbvivo.Text = "Vivo";
+            this.cbvivo.UseVisualStyleBackColor = true;
+            this.cbvivo.CheckedChanged += new System.EventHandler(this.cbvivo_CheckedChanged);
+            // 
+            // cboppo
+            // 
+            this.cboppo.AutoSize = true;
+            this.cboppo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboppo.Location = new System.Drawing.Point(223, 82);
+            this.cboppo.Name = "cboppo";
+            this.cboppo.Size = new System.Drawing.Size(84, 28);
+            this.cboppo.TabIndex = 5;
+            this.cboppo.Text = "Oppo";
+            this.cboppo.UseVisualStyleBackColor = true;
+            this.cboppo.CheckedChanged += new System.EventHandler(this.cboppo_CheckedChanged);
+            // 
+            // cbxiaomi
+            // 
+            this.cbxiaomi.AutoSize = true;
+            this.cbxiaomi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxiaomi.Location = new System.Drawing.Point(223, 133);
+            this.cbxiaomi.Name = "cbxiaomi";
+            this.cbxiaomi.Size = new System.Drawing.Size(97, 28);
+            this.cbxiaomi.TabIndex = 6;
+            this.cbxiaomi.Text = "Xiaomi";
+            this.cbxiaomi.UseVisualStyleBackColor = true;
+            this.cbxiaomi.CheckedChanged += new System.EventHandler(this.cbxiaomi_CheckedChanged);
+            // 
+            // cbrealme
+            // 
+            this.cbrealme.AutoSize = true;
+            this.cbrealme.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbrealme.Location = new System.Drawing.Point(223, 186);
+            this.cbrealme.Name = "cbrealme";
+            this.cbrealme.Size = new System.Drawing.Size(103, 28);
+            this.cbrealme.TabIndex = 7;
+            this.cbrealme.Text = "Realme";
+            this.cbrealme.UseVisualStyleBackColor = true;
+            this.cbrealme.CheckedChanged += new System.EventHandler(this.cbrealme_CheckedChanged);
+            // 
+            // cbrog
+            // 
+            this.cbrog.AutoSize = true;
+            this.cbrog.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbrog.Location = new System.Drawing.Point(223, 247);
+            this.cbrog.Name = "cbrog";
+            this.cbrog.Size = new System.Drawing.Size(89, 28);
+            this.cbrog.TabIndex = 8;
+            this.cbrog.Text = "R.O.G";
+            this.cbrog.UseVisualStyleBackColor = true;
+            this.cbrog.CheckedChanged += new System.EventHandler(this.cbrog_CheckedChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(120, 311);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(112, 40);
+            this.btnSubmit.TabIndex = 9;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 400);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 21);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Enable";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnmybutton
+            // 
+            this.btnmybutton.Enabled = false;
+            this.btnmybutton.Location = new System.Drawing.Point(155, 392);
+            this.btnmybutton.Name = "btnmybutton";
+            this.btnmybutton.Size = new System.Drawing.Size(108, 34);
+            this.btnmybutton.TabIndex = 11;
+            this.btnmybutton.Text = "My Button";
+            this.btnmybutton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 454);
+            this.Controls.Add(this.btnmybutton);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.cbrog);
+            this.Controls.Add(this.cbrealme);
+            this.Controls.Add(this.cbxiaomi);
+            this.Controls.Add(this.cboppo);
+            this.Controls.Add(this.cbvivo);
+            this.Controls.Add(this.cbsamsung);
+            this.Controls.Add(this.cbapple);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboneplus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cboneplus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbapple;
+        private System.Windows.Forms.CheckBox cbsamsung;
+        private System.Windows.Forms.CheckBox cbvivo;
+        private System.Windows.Forms.CheckBox cboppo;
+        private System.Windows.Forms.CheckBox cbxiaomi;
+        private System.Windows.Forms.CheckBox cbrealme;
+        private System.Windows.Forms.CheckBox cbrog;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnmybutton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Check_Box
+{
+    public partial class Form1 : Form
+    {
+        string Text1 = "";
+        string Text2 = "";
+        string Text3 = "";
+        string Text4 = "";
+        string Text5 = "";
+        string Text6 = "";
+        string Text7 = "";
+        string Text8 = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboneplus_CheckedChanged(object sender, EventArgs e)
+        {
+            if(cboneplus.Checked == true)
+            {
+                Text1 = cboneplus.Text;
+            }
+            else
+            {
+                Text1 = "";
+            }
+
+        }
+
+        private void cbapple_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbapple.Checked == true)
+            {
+                Text2 = cbapple.Text;
+            }
+            else
+            {
+                Text2 = "";
+            }
+        }
+
+        private void cbsamsung_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbsamsung.Checked == true)
+            {
+                Text3 = cbsamsung.Text;
+            }
+            else
+            {
+                Text3 = "";
+            }
+        }
+
+        private void cbvivo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbvivo.Checked == true)
+            {
+                Text4 = cbvivo.Text;
+            }
+            else
+            {
+                Text4 = "";
+            }
+        }
+
+        private void cboppo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cboppo.Checked == true)
+            {
+                Text5 = cboppo.Text;
+            }
+            else
+            {
+                Text5 = "";
+            }
+        }
+
+        private void cbxiaomi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbxiaomi.Checked == true)
+            {
+                Text6 = cbxiaomi.Text;
+            }
+            else
+            {
+                Text6 = "";
+            }
+
+        }
+
+        private void cbrealme_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbrealme.Checked == true)
+            {
+                Text7 = cbrealme.Text;
+            }
+            else
+            {
+                Text7 = "";
+            }
+
+        }
+
+        private void cbrog_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbrog.Checked == true)
+            {
+                Text8 = cbrog.Text;
+            }
+            else
+            {
+                Text8 = "";
+            }
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show("Your Favorite Mobile companies are: " + Text1 + " " + Text2 + " " + Text3 + " " + Text4 +
+                " " + Text5 + " " + Text6 + " " + Text7 + " " + Text8 );
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            btnmybutton.Enabled = checkBox1.Checked;
+        }
+    }
+}
